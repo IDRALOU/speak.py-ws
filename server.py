afc9b11922e14bede4f2777896362d7fc4facf91
IDRALOU <54266864+IDRALOU@users.noreply.github.com>
2021-05-25 20:56:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,38 +1,54 @@
 from quart import Quart, websocket
+import json
 
 app = Quart(__name__)
 clients = []
 nicknames = []
 
-async def broadcast(message):
+async def broadcast(message, username):
     for client in clients:
-        await client.send(message)
+        await client.send(json.dumps({"type": "message", "username": username, "message": message}))
+
+async def send_new_connection(username):
+    for client in clients:
+        await client.send(json.dumps({"type": "connection", "username": username}))
+
+async def send_leave(username):
+    for client in clients:
+        await client.send(json.dumps({"type": "leave", "username": username}))
 
 @app.route("/")
 async def home():
     return "Salut !"
 
-@app.websocket("/ws")
-async def message():
+@app.websocket("/")
+async def ws():
     message_new_connection = await websocket.receive_json()
     if message_new_connection["username"] in nicknames:
         await websocket.send("nickname déjà enregistré")
         return
+    elif message_new_connection["event"] != "connect":
+        await websocket.send("close")
+        return
     clients.append(websocket._get_current_object())
     nicknames.append(message_new_connection["username"])
-    await broadcast(f"{message_new_connection['username']} a rejoint le salon.")
+    await send_new_connection(message_new_connection["username"])
     try:
         while True:
-            message = await websocket.receive()
-            if not message == "nickname déjà enregistré":
-                await broadcast(message)
+            message = await websocket.receive_json()
+            if message["username"] not in nicknames:
+                await websocket.send("close")
+                break
+            if message["event"] == "post message":
+                if message["message"] != "nickname déjà enregistré":
+                    await broadcast(message["message"], message["username"])
     except:
         if websocket._get_current_object() in clients:
             index_client = clients.index(websocket._get_current_object())
             clients.remove(websocket._get_current_object())
             nickname = nicknames[index_client]
             nicknames.remove(nickname)
-            await broadcast(f"{message_new_connection['username']} a quitté le salon.")
+            await send_leave(nickname)
 
 
 
